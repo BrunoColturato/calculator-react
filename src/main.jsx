653a8036777024aff3f1998f29ebb5b4be94e879
BrunoColturato <brcolturato@gmail.com>
2023-06-16 10:23:31 -0300

    Last styles updates

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Calculator from './main/Calculator.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Calculator from "./main/Calculator.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Calculator />
-  </React.StrictMode>,
-)
+    <div className="main">
+      <Calculator />
+      <footer>
+        <p>
+          Coded by{" "}
+          <a href="https://brunocolturato.github.io/" target="_blank">
+            Bruno Alvarenga Colturato
+          </a>
+          .
+        </p>
+      </footer>
+    </div>
+  </React.StrictMode>
+);
